aa3131bdaa5cfe3a26fcad72282c12b520826a2d
Anissa Schirock <schirock.a@gmail.com>
2018-09-26 13:15:11 +0200

    added solution

--- Assets/Scripts/Pathfinding.cs
+++ Assets/Scripts/Pathfinding.cs
@@ -15,11 +15,47 @@ public class Pathfinding : MonoBehaviour {
 		FindPath (seeker.position, target.position);
 	}
 
-	void FindPath(Vector3 startPos, Vector3 targetPos) {
-		//TODO: Write A* Algorithm here
-		//Hint: Get the start and end node with grid.NodeFromWorldPoint();
-		
-	
+		void FindPath(Vector3 startPos, Vector3 targetPos) {
+		Node startNode = grid.NodeFromWorldPoint(startPos);
+		Node targetNode = grid.NodeFromWorldPoint(targetPos);
+
+		List<Node> openSet = new List<Node>();
+		HashSet<Node> closedSet = new HashSet<Node>();
+		openSet.Add(startNode);
+
+		while (openSet.Count > 0) {
+			Node node = openSet[0];
+			for (int i = 1; i < openSet.Count; i ++) {
+				if (openSet[i].fCost < node.fCost || openSet[i].fCost == node.fCost) {
+					if (openSet[i].hCost < node.hCost)
+						node = openSet[i];
+				}
+			}
+
+			openSet.Remove(node);
+			closedSet.Add(node);
+
+			if (node == targetNode) {
+				RetracePath(startNode,targetNode);
+				return;
+			}
+
+			foreach (Node neighbour in grid.GetNeighbours(node)) {
+				if (!neighbour.walkable || closedSet.Contains(neighbour)) {
+					continue;
+				}
+
+				int newCostToNeighbour = node.gCost + GetDistance(node, neighbour);
+				if (newCostToNeighbour < neighbour.gCost || !openSet.Contains(neighbour)) {
+					neighbour.gCost = newCostToNeighbour;
+					neighbour.hCost = GetDistance(neighbour, targetNode);
+					neighbour.parent = node;
+
+					if (!openSet.Contains(neighbour))
+						openSet.Add(neighbour);
+				}
+			}
+		} 
 	}
 
 	void RetracePath(Node startNode, Node endNode) {
